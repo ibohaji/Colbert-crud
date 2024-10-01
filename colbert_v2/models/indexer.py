803b65074d796ea08b632585f64c7047a4584f1c
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-01 11:25:04 +0200

    proper header

--- colbert_v2/models/indexer.py
+++ colbert_v2/models/indexer.py
@@ -9,8 +9,8 @@ from data.collection import FileProcessor
 class ColBERTIndexer:
     def __init__(self, config, collection_path):
         self.config = config
-
         FileProcessor.add_missing_headers(config.COLLECTION_PATH)
+        FileProcessor.ensure_proper_header(config.COLLECTION_PATH)
 
         self.queries = Queries(path=config.QUERIES_PATH)
         self.collection = Collection(path=config.COLLECTION_PATH)
