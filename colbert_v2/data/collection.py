6409f65d47fdf279cb57d01ebc748f309c01dd05
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-01 11:41:35 +0200

    file

--- colbert_v2/data/collection.py
+++ colbert_v2/data/collection.py
@@ -26,21 +26,18 @@ class FileProcessor:
     @staticmethod
     def add_missing_headers(input_file):
         with open(input_file, 'r') as f_in:
-             lines = f_in.readlines()  # Store the contents of the file
+            lines = f_in.readlines()
 
         with open(input_file, 'w') as f_out:
-            # Check if the first line has the 'id' and 'text' headers
-            if not lines[0].startswith("id\ttext"):
-                f_out.write("id\ttext\n")  # Write the header if missing
-                lines = lines[1:]  # Skip the first line since we added the header
-
-            # Update the 'id' to match the line number and write the content back
             for line_idx, line in enumerate(lines):
-                # Split the line by tab, replace the 'pid' with the line number
-                parts = line.strip().split('\t', 1)
-                if len(parts) == 2:
-                    f_out.write(f"{line_idx}\t{parts[1]}\n")
-
+                # If it's the first line and it's not already the header, add the header
+                if line_idx == 0 and line.strip() != "id\ttext":
+                    f_out.write("id\ttext\n")
+                else:
+                    # For all other lines, split by tab, replace the first value with the line index
+                    parts = line.strip().split('\t', 1)
+                    if len(parts) == 2:
+                        f_out.write(f"{line_idx}\t{parts[1]}\n")
 
 
     def save_new_document(self, new_document):
