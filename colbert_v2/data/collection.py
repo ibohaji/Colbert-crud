63c525baf3c920772a978874af77d439e221c770
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-01 11:17:12 +0200

    file

--- colbert_v2/data/collection.py
+++ colbert_v2/data/collection.py
@@ -14,19 +14,17 @@ class FileProcessor:
     @staticmethod
     def add_missing_headers(input_file):
         with open(input_file, 'r') as f_in:
-            first_line = f_in.readline().strip()
+            lines = f_in.readlines()  # Store the contents of the file
 
+        # Open the file again in write mode
+        with open(input_file, 'w') as f_out:
             # Check if the first line has the 'id' and 'text' headers
-            if not first_line.startswith("id\ttext"):
-                with open(input_file, 'w') as f_out:
-                    f_out.write("id\ttext\n")  # Add header
-                    f_out.write(first_line + '\n')  # Write the first line as data
-                    f_out.writelines(f_in)  # Write the rest of the file
-            else:
-                # If the header exists, just copy the file as-is
-                with open(input_file, 'w') as f_out:
-                    f_out.write(first_line + '\n')
-                    f_out.writelines(f_in)
+            if not lines[0].startswith("id\ttext"):
+                f_out.write("id\ttext\n")  # Write header if missing
+            
+            # Write back the original content
+            f_out.writelines(lines)
+
 
 
 
