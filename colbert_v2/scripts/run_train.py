f7c928ea574ca5fceeec15a1b6f8cfd7e0a922bd
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-02 19:30:13 +0200

    data_pre

---
+++ colbert_v2/scripts/run_train.py
@@ -0,0 +1,19 @@
+# scripts/run_train.py
+from colbert_v2 import Trainer, create_dataloader
+from colbert_v2.model.indexer import Encoder
+
+# Load data, create encoder and dataloader
+texts, labels = load_your_data()  # Dummy function
+dataloader = create_dataloader(texts, labels)
+
+encoder = Encoder()
+trainer = Trainer(encoder, dataloader)
+trainer.train()
+
+# scripts/run_eval.py
+from colbert_v2 import Evaluator
+
+# Dummy data
+true_labels = ...
+pred_scores = ...
+print(Evaluator.ndcg_score(true_labels, pred_scores))
---
+++ colbert_v2/scripts/run_update.py
@@ -0,0 +1,16 @@
+# scripts/run_update.py
+from models.updater import ColBERTUpdater
+from models.searcher import ColBERTSearcher
+from config import Config
+
+if __name__ == "__main__":
+    searcher = ColBERTSearcher(Config.INDEX_NAME)
+    updater = ColBERTUpdater(Config(), searcher)
+    
+    # Adding new documents
+    new_docs = ["New document 1", "New document 2"]
+    updater.add_documents(new_docs)
+
+    # Removing documents by ID
+    remove_ids = [0, 1]
+    updater.remove_documents(remove_ids)
