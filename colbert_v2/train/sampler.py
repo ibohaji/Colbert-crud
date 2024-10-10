56a0c25a254e09d60e05394fe3c3705a86084b94
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-10 11:55:00 +0200

    logger

--- colbert_v2/train/sampler.py
+++ colbert_v2/train/sampler.py
@@ -11,16 +11,17 @@ logger = logging.getLogger(__name__)
 
 nltk.download('punkt')
 class HardNegativesSampler:
-    def __init__(self, queries, collection, host='http://localhost:9200'):
+    def __init__(self, queries, collection, host):
+        self.host= host if host else'http://10.66.10.32:9200' 
         self.collection = collection
         self.es = self.setup_es_index()
         self.index_name = 'documents'
         self.queries = queries
-        self.host= 'http://10.66.10.32:9200'
 
     def setup_es_index(self):
         es_searcher = EsSearcher()
         es_searcher.index_documents(self.collection.collection_dict)
+        logger.info(f"Indexed {len(self.collection.collection_dict)} documents into Elasticsearch")
         return es_searcher
 
     def get_hard_negatives_all(self, num_negatives=3):
