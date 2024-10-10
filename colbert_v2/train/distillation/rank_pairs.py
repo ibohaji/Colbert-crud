6736c4e23d2b007f6dd72197280ab012a34d60bf
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-10 21:28:20 +0200

    Colbert module

---
+++ colbert_v2/train/distillation/rank_pairs.py
@@ -0,0 +1,53 @@
+from ....ColBERT.colbert.distillation.scorer import Scorer
+from ....ColBERT.colbert.distillation.ranking_scorer import RankingScorer
+from ....ColBERT.colbert.infra import Run
+from collections import defaultdict
+import wandb
+import argparse
+import json 
+import tqdm
+import ujson
+
+
+def main(qid, pid, collection, queries):
+
+    scorer = Scorer(queries=queries, collection=collection)
+    distillation_scores = scorer.launch(qids, pids)
+
+    scores_by_qid = defaultdict(list)
+    for qid, pid, score in tqdm.tqdm(zip(qids, pids, distillation_scores)):
+        scores_by_qid[qid].append((score, pid))
+
+        with Run().open('distillation_scores.json', 'w') as f:
+            for qid in tqdm.tqdm(scores_by_qid):
+                obj = (qid, scores_by_qid[qid])
+                f.write(ujson.dumps(obj) + '\n')
+
+            output_path = f.name
+            print(f"Saved distillation scores to {output_path}")
+
+
+if __name__=="__main__": 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-- qid_path', type=str, required=True)
+    parser.add_argument('-- pid_path', type=str, required=True)
+    parser.add_argument('--collection_path', type=str, required=True)
+    parser.add_argument('--queries_path', type=str, required=True)
+
+    arg = parser.parse_args()
+
+
+    with open(arg.collection_path, 'r') as f:
+        collection = json.load(f)
+    
+    with open(arg.queries_path, 'r') as f:
+        queries = json.load(f)
+
+
+    with open(arg.pid_path, 'r') as f:
+        pids = json.load(f)
+    
+    with open(arg.qid_path, 'r') as f:
+        qids = json.load(f)
+
+
---
+++ colbert_v2/train/distillation/scores.py
@@ -0,0 +1,48 @@
+from colbert.distillation.scorer import Scorer
+import argparse
+import os
+import json
+import logging
+from colbert_v2.custom.data_organizer import CollectionData, GenQueryData
+import ujson
+
+
+
+def save_triples(scored_triples, output_path):
+    with open(output_path, 'w') as f:
+        for triple in scored_triples:
+            ujson.dump(triple, f)
+            f.write('\n')
+    print(f"Saved triples to {output_path}")
+
+def integrate_scores(triples, scores):
+    return [(query_id, pos_id, neg_id, score) for (query_id, pos_id, neg_id), score in zip(triples, scores)]
+
+
+def compute_scores(triples, queries_data, collection_data):
+    queries = {qid: query['text'] for qid, query in queries_data.queries_dict.items()}
+    collection = {doc['_id']: doc['text'] for doc in collection_data.collection_dict}
+
+    scorer = Scorer(queries, collection, model='cross-encoder/ms-marco-MiniLM-L-6-v2')
+
+    qids = [triple[0] for triple in triples]
+    pids = [triple[1] for triple in triples]
+    
+    scores = scorer.launch(qids, pids)
+    return scores
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--triples_path', type=str, required=True)
+    parser.add_argument('--collection_path', type=str, required=True)
+
+
+    args = parser.parse_args()
+    gen_queries = GenQueryData(args.triples_path)
+    collection_data = CollectionData(args.collection_path)
+    triples = args.triples
+    scores = compute_scores(triples, gen_queries, collection_data)
+    scored_triples = integrate_scores(triples, scores)
+
+    save_triples(scored_triples, 'scored_triples_.json')
