d005018c0224480f650561818ae0381df0546f94
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-13 01:01:58 +0200

    hmm

--- colbert_v2/train/distillation/train.py
+++ colbert_v2/train/distillation/train.py
@@ -20,10 +20,8 @@ def convert_json_file_to_jsonl(input_json_path, output_jsonl_path):
         for key, value in input_json.items():
             jsonl_file.write(json.dumps({"qid": key, "question": value}) + '\n')
 
-
 def json_to_tsv(input_file, output_file):
-    is_jsonl = input_file.endswith('.json')
-
+    is_jsonl = input_file.endswith('.jsonl')  
     with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', newline='', encoding='utf-8') as outfile:
         writer = csv.writer(outfile, delimiter='\t')
 
@@ -31,29 +29,23 @@ def json_to_tsv(input_file, output_file):
             for line in infile:
                 data = json.loads(line)
                 if isinstance(data, dict):
-                    writer.writerow([data.get('id'), data.get('text')])
+                    writer.writerow([data.get('_id'), data.get('text')])
         else:
-            data = json.load(infile)
-            if isinstance(data, list):
-                for item in data:
-                    writer.writerow([item.get('id'), item.get('text')])
-            elif isinstance(data, dict):
+            data = json.load(infile) 
+            if isinstance(data, dict):
                 for key, value in data.items():
-                    writer.writerow([key, value])
+                    writer.writerow([value.get('_id'), value.get('text')])
+
 
 
 
 def run_distillation(triples_, queries_, collection_):
-    
 
     with Run().context(RunConfig(nranks=1)):
-        
         config = ColBERTConfig(bsize=32, lr=1e-05, warmup=20_000, doc_maxlen=180, dim=128, attend_to_mask_tokens=False, nway=64, accumsteps=1, similarity='cosine', use_ib_negatives=True)
         trainer = Trainer(triples=triples_, queries=queries_, collection=collection_, config=config)
         trainer.train(checkpoint='colbert-ir/colbertv1.9')
         
-
-
 
 
 if __name__ == "__main__":
@@ -74,4 +66,3 @@ if __name__ == "__main__":
     json_to_tsv(collection, output_path_collection)
     convert_json_file_to_jsonl(queries, output_path_queries)
     run_distillation(triples, output_path_queries, output_path_collection)
-
