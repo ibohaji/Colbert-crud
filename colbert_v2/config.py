dec4cac9e26bc11db120096ad72734cd76518576
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-15 02:22:29 +0200

    singleton

--- colbert_v2/config.py
+++ colbert_v2/config.py
@@ -41,7 +41,7 @@ class MetaData():
 
 def Singleton(type):
     _instances = {}
-    def __new__(cls, *args, **kwargs):
+    def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return _instances[cls]
