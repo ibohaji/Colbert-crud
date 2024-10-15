32aeb4efe66602ce2ffb20d2fc023b4ebec19365
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-15 02:19:08 +0200

    singleton

--- colbert_v2/config.py
+++ colbert_v2/config.py
@@ -43,7 +43,7 @@ def Singleton(cls):
     _instances = {}
     def __new__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__new__(cls)
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return _instances[cls]
 
 
