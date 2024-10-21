64196389be11ce7e09ca14c758ca145c06a043b1
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-21 02:27:22 +0200

    typo

--- colbert_v2/train/query_generator/query_generator.py
+++ colbert_v2/train/query_generator/query_generator.py
@@ -72,94 +72,36 @@ class QueryGenerator:
     @classmethod
     def _removeNonAscii(s:str) -> str : return "".join(i for i in s if ord(i) < 128)
 
-    def generate_queries(self,batch_size:int = 16)->None:
-        generated_queries = {}
-        qrel = defaultdict(list)
-        doc_items = [(doc_id, doc) for doc_id, doc in self.documents.items()]
 
-        if self.output_path:
-            os.makedirs(self.output_path, exist_ok=True)
-        else:
-            self.output_path = "generated_query_data"
-            os.makedirs(self.output_path, exist_ok=True)
+    def generate_split_queries(self, generated_queries: Dict[int, str], qrel: Dict[int, int], hold_out_size: int = 2):
+        
+        train_queries = {}
+        validation_queries = {}
 
-            for start_idx in tqdm(range(0,len(doc_items),batch_size), desc="Generating queries"):
-                batch_docs = doc_items[start_idx:start_idx+batch_size]
+        queries_by_doc = defaultdict(list)
+        for query_id, doc_id in qrel.items():
+            queries_by_doc[doc_id].append(query_id)
+        
+        for doc_id, query_ids in queries_by_doc.items():
+            if len(query_ids) <= hold_out_size + 1:
+                validation_queries.update({qid: generated_queries[qid] for qid in query_ids})
+            else:
+                validation_queries.update({qid: generated_queries[qid] for qid in query_ids[:hold_out_size]})
+                train_queries.update({qid: generated_queries[qid] for qid in query_ids[hold_out_size:]})
+        
+        validation_output_file = os.path.join(self.output_path, "validation_queries.tsv")
+        train_output_file = os.path.join(self.output_path, "train_queries.tsv")
+        
+        self.save_queries_to_tsv(validation_queries, validation_output_file)
+        self.save_queries_to_tsv(train_queries, train_output_file)
 
-                for doc_id, doc in batch_docs:
-                    print('doc before  cleeaning:',doc)
-                    print('doc id:',doc_id)
-                    doc = self._clean_text(doc)
-                    with open('cleaned_docs.txt','w') as f:
-                        f.write(doc)
-                        
-                    return doc
-                    print('doc after cleaning:',doc)
-                    print('\nThe batch docs are\n,',batch_docs)
-                    print('Doc ids are \n',doc_id)
-                    print('Docs are \n',doc)
-                    break
-                    input_ids = self.tokenizer.encode(
-                        doc,
-                        max_length=512,
-                        truncation=True,
-                        return_tensors='pt'
-                        ).to(self.device)
-
-                    outputs = self.model.generate(
-                        input_ids=input_ids,
-                        max_length=64,
-                        do_sample=True,
-                        top_p=0.95,
-                        num_return_sequences=5
-                    )
-
-                queries = [self._clean_text(self.tokenizer.decode(output, skip_special_tokens=True)) for output in outputs]
-                queries = self.remove_duplicates(queries)
-                queries_id = self.generate_query_ids(len(queries))
-                
-                for _qid, query_text in zip(queries_id, queries):
-                    generated_queries[_qid] = query_text
-                    qrel[_qid] = doc_id
-
-                
-
-
-        self.save_queries_to_json(generated_queries, f"{self.output_path}/generated_queries.json")
-        self.save_queries_to_json(qrel, f"{self.output_path}/qrel.json")
-        self.generate_split_queries(generated_queries)
-        return generated_queries
-
-
-def generate_split_queries(self, generated_queries: Dict[int, str], qrel: Dict[int, int], hold_out_size: int = 2):
+        print(f"TSV files saved: {len(train_queries)} training queries and {len(validation_queries)} validation queries.")
+        
+    def save_queries_to_tsv(self, queries: Dict[int, str], output_file: str):
     
-    train_queries = {}
-    validation_queries = {}
-
-    queries_by_doc = defaultdict(list)
-    for query_id, doc_id in qrel.items():
-        queries_by_doc[doc_id].append(query_id)
-    
-    for doc_id, query_ids in queries_by_doc.items():
-        if len(query_ids) <= hold_out_size + 1:
-            validation_queries.update({qid: generated_queries[qid] for qid in query_ids})
-        else:
-            validation_queries.update({qid: generated_queries[qid] for qid in query_ids[:hold_out_size]})
-            train_queries.update({qid: generated_queries[qid] for qid in query_ids[hold_out_size:]})
-    
-    validation_output_file = os.path.join(self.output_path, "validation_queries.tsv")
-    train_output_file = os.path.join(self.output_path, "train_queries.tsv")
-    
-    self.save_queries_to_tsv(validation_queries, validation_output_file)
-    self.save_queries_to_tsv(train_queries, train_output_file)
-
-    print(f"TSV files saved: {len(train_queries)} training queries and {len(validation_queries)} validation queries.")
-    
-def save_queries_to_tsv(self, queries: Dict[int, str], output_file: str):
- 
-    with open(output_file, 'w', encoding='utf-8') as f:
-        for qid, qtext in queries.items():
-            f.write(f"{qid}\t{qtext}\n")
+        with open(output_file, 'w', encoding='utf-8') as f:
+            for qid, qtext in queries.items():
+                f.write(f"{qid}\t{qtext}\n")
         
     def remove_duplicates(self, generated_queries:list):
         return list(set([query.lower() for query in generated_queries]))
@@ -178,6 +120,66 @@ def save_queries_to_tsv(self, queries: Dict[int, str], output_file: str):
     def _clean_text(self, text):
         """Utility function to clean text by removing tabs and non-ASCII characters."""
         return text.replace("\t", " ").encode("ascii", "ignore").decode().strip()
+
+
+        def generate_queries(self,batch_size:int = 16)->None:
+            generated_queries = {}
+            qrel = defaultdict(list)
+            doc_items = [(doc_id, doc) for doc_id, doc in self.documents.items()]
+
+            if self.output_path:
+                os.makedirs(self.output_path, exist_ok=True)
+            else:
+                self.output_path = "generated_query_data"
+                os.makedirs(self.output_path, exist_ok=True)
+
+                for start_idx in tqdm(range(0,len(doc_items),batch_size), desc="Generating queries"):
+                    batch_docs = doc_items[start_idx:start_idx+batch_size]
+
+                    for doc_id, doc in batch_docs:
+                        print('doc before  cleeaning:',doc)
+                        print('doc id:',doc_id)
+                        doc = self._clean_text(doc)
+                        with open('cleaned_docs.txt','w') as f:
+                            f.write(doc)
+                            
+                        return doc
+                        print('doc after cleaning:',doc)
+                        print('\nThe batch docs are\n,',batch_docs)
+                        print('Doc ids are \n',doc_id)
+                        print('Docs are \n',doc)
+                        break
+                        input_ids = self.tokenizer.encode(
+                            doc,
+                            max_length=512,
+                            truncation=True,
+                            return_tensors='pt'
+                            ).to(self.device)
+
+                        outputs = self.model.generate(
+                            input_ids=input_ids,
+                            max_length=64,
+                            do_sample=True,
+                            top_p=0.95,
+                            num_return_sequences=5
+                        )
+
+                    queries = [self._clean_text(self.tokenizer.decode(output, skip_special_tokens=True)) for output in outputs]
+                    queries = self.remove_duplicates(queries)
+                    queries_id = self.generate_query_ids(len(queries))
+                    
+                    for _qid, query_text in zip(queries_id, queries):
+                        generated_queries[_qid] = query_text
+                        qrel[_qid] = doc_id
+
+                    
+
+
+            self.save_queries_to_json(generated_queries, f"{self.output_path}/generated_queries.json")
+            self.save_queries_to_json(qrel, f"{self.output_path}/qrel.json")
+            self.generate_split_queries(generated_queries)
+            return generated_queries
+
 
         
 if __name__ =="__main__":
