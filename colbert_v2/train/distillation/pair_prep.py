a64dc7610fc7a5246dabb9c803b2977c7a4012cc
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-22 17:07:56 +0200

    ES HELL! debugging

--- colbert_v2/train/distillation/pair_prep.py
+++ colbert_v2/train/distillation/pair_prep.py
@@ -46,7 +46,7 @@ def main(generated_queries_path, collection_path, host, out_path, num_negatives=
         queries_data = GenQueryData(generated_queries_path)
         collection_data = CollectionData(collection_path)
 
-        hard_negatives = HardNegativesSampler(queries=queries_data, collection=collection_data).get_hard_negatives_all(num_negatives=num_negatives)
+        hard_negatives = HardNegativesSampler(queries=queries_data, collection=collection_data, es=es_searcher ).get_hard_negatives_all(num_negatives=num_negatives)
         qids, pids = generate_pairs(queries_data, hard_negatives)
 
 
