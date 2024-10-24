d9636752fa2694dac7f09a577073e2877cdcc48e
ibohaji <ibrahimdeiaa@gmail.com>
2024-10-24 16:38:11 +0200

    data_loader issues

--- colbert_v2/train/distillation/train.py
+++ colbert_v2/train/distillation/train.py
@@ -96,8 +96,6 @@ def run_distillation(triples_, queries_, collection_, experiment):
 
     with Run().context(RunConfig(nranks=1, experiment=experiment)):
         config = ColBERTConfig(bsize=32, lr=LR, warmup=20_000, accumsteps=1, doc_maxlen=512, dim=128, attend_to_mask_tokens=False, nway=64, similarity='cosine', use_ib_negatives=True)
-        config.checkpoint  = 'colbert-ir/colbert-v1.2-msmarco'
-        print(f"Config.maxsteps: {config.maxsteps}")
         trainer = Trainer(triples=triples_, queries=queries_, collection=collection_, config=config)
         trainer.train(checkpoint='colbert-ir/colbertv1.9')
 
